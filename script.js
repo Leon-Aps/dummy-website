22f000c69159e814471252d02e4582cd4f398faf
Leon-Aps <sumtergas@gmail.com>
2025-01-19 19:54:54 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const message = document.createElement("p");
+    message.textContent = "Bedankt voor het bezoeken van deze website!";
+    message.style.color = "#0d6efd";
+    message.style.textAlign = "center";
+    document.body.appendChild(message);
+});
